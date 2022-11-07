6afaef05437233e29dcce5004b063f4d18d2416c
Kerinus Brazauskas <brazauskaskernius@gmail.com>
2022-11-07 17:57:21 +0200

    skaidinys size change

--- test.py
+++ test.py
@@ -4,14 +4,18 @@ from PIL import Image
 import PIL
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-im = Image.open('small2.jpg', 'r')
+skaidinysSize = 10
+
+im = Image.open('big.jpg', 'r')
 width, height = im.size
 pixel_values = list(im.getdata())
 
-im2 = Image.open('small3.jpg', 'r')
+im2 = Image.open('big.jpg', 'r')
 width2, height2 = im2.size
 pixel_values2 = list(im2.getdata())
+
 
 def getBrightness(pixelValues):
     brightnessArray = []
@@ -19,16 +23,26 @@ def getBrightness(pixelValues):
     for x in pixelValues:
         rgbSum = x[0] + x[1] + x[2]
         brightness = rgbSum / 3
-        brightnessArray.append(brightness)  
+        brightnessArray.append(brightness)
     return brightnessArray
+
 
 brightnessArray = getBrightness(pixel_values)
 brightnessArray2 = getBrightness(pixel_values2)
 
+
+def current_milli_time():
+    return round(time.time() * 1000)
+
+
 def showImage(array):
     image_array = np.array(array, dtype=np.uint8)
     img = PIL.Image.fromarray(image_array)
-    img.show()
+
+    current_time = str(current_milli_time())
+
+    img.save(current_time+'.jpg')
+
 
 def prepareForViewing(brightnessArray):
     l = []
@@ -47,8 +61,9 @@ def prepareForViewing(brightnessArray):
             w += 1
         h += 1
         l.append(tempLine)
-    
+
     showImage(l)
+
 
 prepareForViewing(brightnessArray)
 prepareForViewing(brightnessArray2)
@@ -66,56 +81,62 @@ def getSkaidinys(brightnessArray):
         while w < width:
             index = h * width + w
 
-            value = round(brightnessArray[index])
-            value2 = round(brightnessArray[index+1])
-            value3 = round(brightnessArray[index+width])
-            value4 = round(brightnessArray[index+width+1])#cia skaidinys
+            count = 0
+            sum = 0
 
-            rounded = (value + value2 + value3 + value4)/4
+            for i in range(0, skaidinysSize):
+                for j in range(0, skaidinysSize):
+                    count += 1
+                    sum += round(brightnessArray[index+j+(width*i)])
+
+            rounded = sum/count
 
             temp2 = [rounded, rounded, rounded]
             tempLine2.append(temp2)
-            w += 2#cia skaidinys
-        h += 2#cia skaidinys
+            w += skaidinysSize
+        h += skaidinysSize
         l2.append(tempLine2)
 
     showImage(l2)
     return l2
 
-l2 = getSkaidinys(brightnessArray)
-belekas = getSkaidinys(brightnessArray2)
+
+skaidinys = getSkaidinys(brightnessArray)
+skaidinys2 = getSkaidinys(brightnessArray2)
+
 
 def findMax(array1, array2):
-    maxDiff=0
-    maxDiffX=0
+    maxDiff = 0
+    maxDiffX = 0
 
-    i=0
-    while i<len(array1):
-        if(abs(array1[i]-array2[i])>maxDiff):
-            maxDiff= abs(array1[i]-array2[i])
+    i = 0
+    while i < len(array1):
+        if (abs(array1[i]-array2[i]) > maxDiff):
+            maxDiff = abs(array1[i]-array2[i])
             maxDiffX = i
-        i+=1
+        i += 1
     print("Maksimalus skirtumas tarp tasku: ", maxDiff)
-    if(maxDiffX==0):
-        x=0
-        y=0
+    if (maxDiffX == 0):
+        x = 0
+        y = 0
     else:
-        x = maxDiffX%(width/2) #cia skaidinys
-        y = maxDiffX//(width/2) #cia skaidinys
+        x = maxDiffX % (width/skaidinysSize)
+        y = maxDiffX//(width/skaidinysSize)
 
     print("X: ", x)
     print("Y: ", y)
 
+
 def drawGraph(doubleArray, doubleArray2):
-    x = list(range(1, len(doubleArray)* len(doubleArray[0])+1))
-    y=[]
+    x = list(range(1, len(doubleArray) * len(doubleArray[0])+1))
+    y = []
     for w in doubleArray:
         for h in w:
             y.append(h[0])
 
-    # plotting the points 
-    plt.plot(x, y, label = "line 1")
-    # plt.scatter(x, y, label= "first", color= "green", 
+    # plotting the points
+    plt.plot(x, y, label="line 1")
+    # plt.scatter(x, y, label= "first", color= "green",
     #         marker= ".", s=1)
 
     # line 2 points
@@ -126,26 +147,26 @@ def drawGraph(doubleArray, doubleArray2):
         for h in w:
             y2.append(h[0])
 
-    # plotting the line 2 points 
-    plt.plot(x2, y2, label = "line 2")
-    # plt.scatter(x2, y2, label= "second", color= "blue", 
+    # plotting the line 2 points
+    plt.plot(x2, y2, label="line 2")
+    # plt.scatter(x2, y2, label= "second", color= "blue",
     #         marker= ".", s=1)
-    
+
     # naming the x axis
     plt.xlabel('x - axis')
     # naming the y axis
     plt.ylabel('y - axis')
     # giving a title to my graph
     plt.title('Two lines on same graph!')
-    
+
     # show a legend on the plot
     plt.legend()
-    
+
     # function to show the plot
     plt.show()
 
     findMax(y, y2)
-  
+
 
 # pip install matplotlib
-drawGraph(l2, belekas)
+drawGraph(skaidinys, skaidinys2)
